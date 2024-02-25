bb541692f37459a3ab1eb24b390dc67660323238
Aram Prazyan <156605373+Pr47y4n@users.noreply.github.com>
2024-02-25 10:28:10 +0400

    Add files via upload

---
+++ hover.js
@@ -0,0 +1,7 @@
+function changeColor(item) {
+    item.style.color = "red"; // Change color to red on mouseover
+}
+
+function resetColor(item) {
+    item.style.color = "black"; // Reset color to black on mouseout
+}
